10eea6e742fc2f83d9725148461a7b3185a33523
Kelvin <kmr19972015@gmail.com>
2020-07-11 14:57:34 -0500

    Front-end del crud MVC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+//Importar archivos
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Clientes from './components/Clientes'
+import NuevoCliente from './components/NuevoCliente';
+import Editarcliente from './components/EditarCliente'
 
+
+//Funcion principal
 function App() {
+  //State primer parametro es el state y el segundo es el modificador del state
+  // const  [Variable_Que_Toma_el_Estado, Funcion_Que modifica el estade de la variable] = useState(Valor de la variable o estado)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+    <div className="container-fluid">
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Clientes} />
+        <Route exact path="/nuevoCliente" component={NuevoCliente} />
+      </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
